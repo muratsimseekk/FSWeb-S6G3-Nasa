e281220779ab849af208f49077064b0c401a2d62
muratsimseekk <muratsimseekk@gmail.com>
2023-10-26 21:50:42 +0300

    proje son hal

--- src/bilesenler/Gonderiler/Video.js
+++ src/bilesenler/Gonderiler/Video.js
@@ -4,11 +4,19 @@ const Video = (props) => {
   const { data } = props;
   //src kısmı dolacak videonun kaynak adresi
   return (
-    <div>
+    <div className="videoDiv">
       <h1>{data.title}</h1>
-      <iframe width={"100%"} height={"400px"} src={data.mediaUrl}></iframe>
-      <p>{data.explanation}</p>
-      <p>{data.copyright}</p>
+      <img
+        style={{ opacity: "0.6" }}
+        src="https://s3.amazonaws.com/digitaltrends-uploads-prod/2015/08/black-hole.jpg"
+      ></img>
+      <iframe src={data.mediaUrl}></iframe>
+      <img
+        style={{ opacity: ".8" }}
+        src="https://wallpaperaccess.com/full/2867611.jpg"
+      ></img>
+      {/* <p>{data.explanation}</p>
+      <p>{data.copyright}</p> */}
     </div>
   );
 };
