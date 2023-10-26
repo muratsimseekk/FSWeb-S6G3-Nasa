d704f544060178beaf2982eca2b443d67fecf9c3
muratsimseekk <muratsimseekk@gmail.com>
2023-10-26 21:43:32 +0300

    3 resim yüklendi  proje tamamlandı

--- src/bilesenler/Gonderiler/ImageViewer.js
+++ src/bilesenler/Gonderiler/ImageViewer.js
@@ -6,11 +6,20 @@ const ImageViewer = (props) => {
   return (
     <div className="imgDiv">
       <h1>{data.title}</h1>
+      <img
+        style={{ opacity: "0.6" }}
+        src="https://s3.amazonaws.com/digitaltrends-uploads-prod/2015/08/black-hole.jpg"
+      ></img>
       <img src={data.url}></img>
-      <p>
+      {/* <p>
         <span className="bold">Explanation:</span>
         {data.explanation}
-      </p>
+      </p> */}
+
+      <img
+        style={{ opacity: ".8" }}
+        src="https://wallpaperaccess.com/full/2867611.jpg"
+      ></img>
       <p className="bold">
         {data.media_type.toUpperCase()} Credit & Copyright {data.copyright}:
       </p>
